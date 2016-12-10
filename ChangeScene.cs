ddd57c297cee89760692b8ecb3fcc6421e22af44
Kameron Montague <kmontague002@gmail.com>
2016-12-10 11:25:47 -0500

    Scripts
    a collection from the Horror apartment game and Udacity projects

---
+++ ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	public void GoToScene(string sceneName){
+		Debug.Log ("Method Called");
+		SceneManager.LoadScene (sceneName);
+	}
+
+
+}
